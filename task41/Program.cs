95100ef28a1c9c92fdb0a76f06b58fe73c921aa3
Aleksandr <tabruka@yandex.ru>
2022-11-18 20:40:52 +0300

    Добавил задачи и gitignore

---
+++ task41/Program.cs
@@ -0,0 +1,62 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.Write("Введите несколько любых чисел: ");
+int[] array = StringToNum(Console.ReadLine());
+Console.Write("Вы ввели следующие числа: ");
+PrintArray(array);
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        count = count + 1;
+    }
+}
+Console.WriteLine();
+Console.WriteLine($"Количество чисел, которые больше нуля: {count}");
+
+
+int[] StringToNum(string input)
+{
+    int count = 1;
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i] == ' ')
+        {
+            count = count + 1;
+        }
+    }
+
+    int[] array = new int [count];
+    int index = 0;
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        string temp = " ";
+
+        while (input [i] != ' ')
+        {
+        if(i != input.Length - 1)
+        {
+            temp = temp + input [i].ToString();
+            i = i  + 1;
+        }
+        else
+        {
+            temp = temp + input [i].ToString();
+            break;
+        }
+        }
+        array[index] = int.Parse(temp);
+        index++;
+    }
+    return array;
+}
+
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
---
+++ task43/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.WriteLine("Введите значение b1");
+double b1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение k1");
+double k1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение b2");
+double b2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение k2");
+double k2 = int.Parse(Console.ReadLine());
+
+double x = (-b2 + b1)/(-k1 + k2);
+double y = k2 * x + b2;
+
+Console.WriteLine($"Точка пересечения двух прямых X: {x}, Y: {y}");
